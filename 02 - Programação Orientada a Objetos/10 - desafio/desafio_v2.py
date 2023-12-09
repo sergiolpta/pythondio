6cee50440b936613c52addc9de9d77e8fa0998dc
Sérgio Guedes Ferreira Braz <42675470+sergiolpta@users.noreply.github.com>
2023-12-09 18:07:15 -0300

    Update desafio_v2.py
    primeiras alterações

--- 02 - Programação Orientada a Objetos/10 - desafio/desafio_v2.py
+++ 02 - Programação Orientada a Objetos/10 - desafio/desafio_v2.py
@@ -1,5 +1,5 @@
 import textwrap
-from abc import ABC, abstractclassmethod, abstractproperty
+from abc import ABC
 from datetime import datetime
 
 
@@ -136,11 +136,9 @@ class Historico:
 
 class Transacao(ABC):
     @property
-    @abstractproperty
     def valor(self):
         pass
 
-    @abstractclassmethod
     def registrar(self, conta):
         pass
 
